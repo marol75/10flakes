93275d90ab41f58000b11e900479fda12826898d
marol75 <98889927+marol75@users.noreply.github.com>
2026-03-28 20:14:44 +0200

    files and flakes

---
+++ configuration.nix
@@ -0,0 +1,50 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    # Pay attention to if you want disk-config.nix or the following for luks
+    ./disk-config2.nix
+    ./users.nix
+    ./networking.nix
+    ./boot.nix
+    ./zram.nix
+    ./impermanenceLUKS.nix
+    # ./impermanence.nix
+  ];
+
+  # After formatting with disko, the following is more robust for LUKS
+  boot.initrd.luks.devices = {
+     cryptroot = {
+       device = "/dev/disk/by-partlabel/luks";
+       allowDiscards = true;
+       preLVM = true;
+     };
+   };
+
+  networking.hostName = "marol"; # Define your hostname.
+
+  # Custom Options
+  custom = {
+    boot.enable = true;
+    users.enable = true;
+    zram.enable = true;
+  };
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+  ];
+
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+}
---
+++ users.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    # Enabled with custom.users.enable = true; in `configuration.nix`
+    custom.users.enable = lib.mkEnableOption "Enables users module";
+  };
+
+  config = lib.mkIf config.custom.users.enable {
+    users.users = {
+      # Change me!
+      marol = {
+        homeMode = "755";
+        isNormalUser = true;
+        # Change me!
+        # description = "marol75";
+        # Change me! generate with `mkpasswd -m SHA-512 -s`
+        # initialHashedPassword = "$6$knlskdQSQp4le3uiy..3$gAUAugTxAeHUpWKf6iwlkasdjf'lkajWNZRTtjbJ4X0PIjkIQOCcLcimOJe4Y0";
+
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+          "root"
+          "marol"
+          "sudo"
+        ];
+        shell = pkgs.zsh;
+        ignoreShellProgramCheck = true;
+        packages = [
+          pkgs.tealdeer
+          pkgs.zoxide
+          pkgs.mcfly
+          pkgs.tokei
+          pkgs.stow
+        ];
+      };
+    };
+    # Best practice, enables `chown -R your-user:your-user`
+    users.groups.marol = {};
+  };
+}
